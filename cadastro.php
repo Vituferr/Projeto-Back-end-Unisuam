b3d42dca2809477f98a5962024cf6453c9224069
Vituferr <126845902+Vituferr@users.noreply.github.com>
2023-10-15 22:42:16 -0300

    Create cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,28 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+    <form method="post">
+        <label>
+            Nome <input type="text" name="nome" id="nome">
+        </label>
+
+        <label>
+            <input type="date" value="">
+        </label>
+
+
+
+    </form>
+    
+</body>
+</html>
