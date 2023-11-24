63c7ea3021eb59125ab8e00ab84c7d2f7497b8fa
Vituferr <126845902+Vituferr@users.noreply.github.com>
2023-11-23 21:24:42 -0300

    little change

---
+++ assets/js/validarform.js
@@ -0,0 +1,175 @@
+const nameInput = document.getElementById('nomelogin');
+const form = document.getElementById('formcad');
+const telInput = document.getElementById('telefone');
+
+
+// Função para formatar numero de telefone celular e fixo.
+function mTel(tel) {
+    tel = tel.replace(/\D/g, "");
+    tel = tel.replace(/^(\d)/, "+$1");
+    tel = tel.replace(/(.{3})(\d)/, "$1($2");
+    tel = tel.replace(/(.{6})(\d)/, "$1)$2");
+    if (tel.length == 12) {
+        tel = tel.replace(/(.{1})$/, "-$1");
+    } else if (tel.length == 13) {
+        tel = tel.replace(/(.{2})$/, "-$1");
+    } else if (tel.length == 14) {
+        tel = tel.replace(/(.{3})$/, "-$1");
+    } else if (tel.length == 15) {
+        tel = tel.replace(/(.{4})$/, "-$1");
+    } else if (tel.length > 15) {
+        tel = tel.replace(/(.{4})$/, "-$1");
+    }
+    return tel;
+}
+
+function formatarTelefone() {
+    const telefoneInput = document.getElementById('telefone');
+    const telefoneFormatado = mTel(telefoneInput.value);
+    telefoneInput.value = telefoneFormatado;
+}
+function formatarTelefone2() {
+    const telefoneInput = document.getElementById('telefoneFixo');
+    const telefoneFormatado = mTel(telefoneInput.value);
+    telefoneInput.value = telefoneFormatado;
+}
+//Fim função de formatação telefone celular e fixo.
+
+// Essa função é para formatar o CPF.
+function ajustaCpf(v) {
+    v = v.replace(/\D/g, "");
+    // Adiciona ponto após os três primeiros números
+    v = v.replace(/^(\d{3})(\d)/, "$1.$2");
+    // Adiciona ponto após os seis primeiros números
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    // Adiciona o hífen antes dos últimos 2 caracteres
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    return v; // Retorna o valor formatado
+}
+
+function formatarCPF() {
+    const cpfInput = document.getElementById('cpf');
+    const cpfFormatado = ajustaCpf(cpfInput.value);
+    cpfInput.value = cpfFormatado;
+}
+//Aqui acaba a função formatação de CPF.
+
+// Aqui é um evento para que o submit comece a função de enviar os dados
+  form.addEventListener("submit" ,(event) => {
+    event.preventDefault();
+    nomeValida()
+    dtnascValida()
+    sexoValida()
+    nomemValida()
+    cpfValida()
+    telcValida()
+    telfValida()
+    enderecoValida()
+    nomelValida()
+    senhaValida()
+    compareSenha()
+    formatarCPF()
+    formatarTelefone()
+    formatarTelefone2()
+    
+    if(nameInput.value === "") {
+    alert("Por favor preencha seu nome");
+    return;
+    }
+})
+
+// form
+const forme = document.querySelector('#formcad')
+// campos
+const campos = document.querySelectorAll('.required')
+// span
+const spans = document.querySelectorAll('.span-')
+
+function setError(index) {
+    campos[index].style.border = '2px solid #e63636'
+    spans[index].style.border = 'block'
+}
+
+function removeError(index) {
+    campos[index].style.border = ''
+    spans[index].style.display = 'none'
+}
+
+function nomeValida(){
+    if(campos[0].value.length < 3) {
+        setError(0)
+    } else {
+        removeError(0)
+    }
+}
+function dtnascValida(){
+    if(campos[1]) {
+        setError(1)
+    } else {
+        removeError(1)
+    }
+}
+function sexoValida(){
+    if(campos[2]) {
+        setError(2)
+    } else {
+        removeError(2)
+    }
+}
+function nomemValida(){
+    if(campos[3].value.length < 6) {
+        setError(3)
+    } else {
+        removeError(3)
+    }
+}
+function cpfValida(){
+    if(campos[4].value.length <= 13) {
+        setError(4)
+    } else {
+        removeError(4)
+    }
+}
+function telcValida(){
+    if(campos[5].value.length < 12) {
+        setError(5)
+    } else {
+        removeError(5)
+    }
+}
+function telfValida(){
+    if(campos[6].value.length < 12) {
+        setError(6)
+    } else {
+        removeError(6)
+    }
+}
+function enderecoValida(){
+    if(campos[7].value.length < 10) {
+        setError(7)
+    } else {
+        removeError(7)
+    }
+}
+function nomelValida(){
+    if(campos[8].value.length < 6) {
+        setError(8)
+    } else {
+        removeError(8)
+    }
+}
+function senhaValida(){
+    if(campos[9].value.length < 6) {
+        setError(9)
+    } else {
+        removeError(9)
+        compareSenha()
+    }
+}
+function compareSenha(){
+    if(campos[9].value == campos[10].vaalue && campos[10].value.length >= 8) {
+        removeError(10)
+    } else {
+    setError(10)
+    }
+}
--- cadastro.php
+++ cadastro.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -7,30 +8,31 @@
     <title>Cadastro</title>
     <link rel="stylesheet" href="assets/css/cadastro.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="script/validarform.js"></script>
+    <script src="assets/js/validarform.js"></script>
     
     
 </head>
 
 <body>
     <div class="main-cadastro">
-        <form action="cadastro.php" id="formcad" method="post">
+        <form action="cadastro_bd.php" id="formcad" method="post">
             <div class="campos-cad">
                 <h1>Faça seu cadastro</h1>
                 <div class="coluna">
                     <div class="lado-esquerdo">
                         <div class="textfield">
                             <label for="nomeloginn"> Nome:</label>
-                            <input type="text" name="nomelogin" id="nomelogin" minlength="15" required>
+                            <input type="text" name="nomelogin" id="nomelogin" class="nome  required" minlength="15" required oninput="nomeValida()">
+                            <span class="span-">Nome completo</span>
                         </div>
 
                         <div class="textfield">
                             <label for="dt-nasc"> Data de Nascimento:</label>
-                            <input type="date" name="dt-nasc" id="dt-nasc" required>
+                            <input type="date" name="dt-nasc" id="dt-nasc" class="dtnasc  required" required oninput="dtnascValida()">
                         </div>
 
                         <div class="textfield">
-                            <div class="sexo">
+                            <div class="sexo   required"  oninput="sexoValida()">
                                 <label for="sexo"> Sexo:</label>
                                 <p><input type="radio" name="sexo" value="masculino" required>Masc.
                                 <input type="radio" name="sexo" value="feminino" required>Fem.
@@ -41,42 +43,50 @@
 
                         <div class="textfield">
                             <label for="mae-log"> Nome da mãe:</label>
-                            <input type="text" name="mae-log" id="mae-log" required>
+                            <input type="text" name="mae-log" id="mae-log" class="nomem  required" required  oninput="nomem()">
+                            <span class="span-">Nome completo</span>
                         </div>
 
                         <div class="textfield">
                             <label for="cpf"> CPF:</label>
-                            <input type="text" name="cpf" id="cpf" maxlength="14" required oninput="formatarCPF()">
+                            <input type="text" name="cpf" id="cpf" class="cpff required" maxlength="14" required oninput="formatarCPF()">
+                            <span class="span-">Insira um cpf válido</span>
                         </div>
                     </div>
                     
                     <div class="lado-direito">
                         <div class="textfield">
                             <label for="telcelular"> Telefone celular:</label>
-                            <input type="text" name="telcelular" id="telefone" maxlength="17" required oninput="formatarTelefone()">
+                            <input type="text" name="telcelular" id="telefone" class="telefonec    required" maxlength="17" required oninput="formatarTelefone()">
+                            <span class="span-">Insira um telefone válido</span>
                         </div>
 
                         <div class="textfield">
                             <label for="telfixo"> Telefone Fixo:</label>
-                            <input type="text" name="telfixo" id="telefoneFixo" maxlength="17" required oninput="formatarTelefone2()">
+                            <input type="text" name="telfixo" id="telefoneFixo" class="telefonef    required" maxlength="17" required oninput="formatarTelefone2()">
+                            <span class="span-">Insira um telefone válido</span>
                         </div>
 
                         <div class="textfield">
                             <label for="endereco"> Endereço:</label>
-                            <input type="text" name="endereco" id="endereco" required>
+                            <input type="text" name="endereco" id="endereco" class="enderecof  required" required oninput="enderecoValida()">
+                            <span class="span-">Insira um endereço válido</span>
                         </div>
 
                         <div class="textfield">
                             <label for="nome-login">Nome login:</label>
-                            <input type="text" name="nome-login" id="nome-login" required>
+                            <input type="text" name="nome-login" id="nome-login" class="nomel   required"  required oninput="nomelValida()">
+                            <span class="span-">Insira no mínimo 6 caracteres</span>
                         </div>
 
                         <div class="textfield">
                             <label for="senhalogin">Senha:</label>
-                            <input type="password" name="senhalogin" id="senhalogin" required>
+                            <input type="password" name="senhalogin" id="senhalogin" class="senhal   required" required   oninput="senhaValida()">
+                            <span class="span-">Insira no mínimo 6 caracteres</span>
 
                             <label for="confirm-senha">Confirme sua senha:</label>
-                            <input type="password" name="confirm-senha" id="confirm-senha" required>
+                            <input type="password" name="confirm-senha" id="confirm-senha" class="senhac    required" required  oninput="compareSenha()">
+                            <span class="span-">A senha deve ser compatível</span>
                         </div>
 
                     </div>
@@ -84,16 +94,20 @@
                 
                 <div class="coluna-btn">
                     <div class="btn-esquerdo">
-                        <button type="submit" name="submit" class="btn-login">Enviar</button>
+                        <button type="submit" name="enviar" class="btn-login">Enviar</button>
                     </div>
                     <div class="btn-direito">
                         <button class="btn-limpar">Limpar</button>
                     </div>
+                    <div class="btn-volta">
+                        <button class="btn-limpar"><a href="index.php">Voltar</a></button>
+                    </div>
+                    
                 </div>
-            </div>
+                 </div>
+                        
         </form>
     </div>
-
-
+                 
 </body>
 </html>
